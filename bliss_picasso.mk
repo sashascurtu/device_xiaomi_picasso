f4750eb4d948d2b8ce9c1ad3ea826d7131727757
Nikita Sorokin <59575896+NikoRos05@users.noreply.github.com>
2020-10-14 21:08:57 +0300

    Time to Bliss

--- bliss_picasso.mk
+++ bliss_picasso.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/ANXCamera/config.mk)
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
@@ -19,7 +19,7 @@ TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_picasso
+PRODUCT_NAME := bliss_picasso
 PRODUCT_DEVICE := picasso
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 5G
@@ -34,13 +34,13 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Build info
-BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys"
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=picasso \
     PRODUCT_NAME=picasso \
-    PRIVATE_BUILD_DESC="picasso-user 10 QKQ1.191222.002/V12.0.2.0.QGICNXM release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RP1A.201005.004 6782484 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
